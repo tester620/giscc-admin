171a87e2e04495a8f4f09ed79f592476e54f65bf
tester620 <dheerajverma82664442@gmail.com>
2026-02-20 16:10:30 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    // baseURL:"https://gicc-server.onrender.com/api/",
-    baseURL:"http://localhost:7777/api/",
+    baseURL:"https://gicc-server.onrender.com/api/",
+    //baseURL:"http://localhost:7777/api/",
 
     withCredentials:true,
 })
